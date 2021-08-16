ee19b2e797e30d100eac1f01330cb5823fa6062e
toumash <fierekwojciech@gmail.com>
2021-08-16 15:38:13 +0200

    Commit 1

---
+++ Main.py
@@ -0,0 +1,8 @@
+from Menu import New_Menu
+
+
+def main():
+    menu = New_Menu()
+
+if __name__ == '__main__':
+    main()
---
+++ Menu.py
@@ -0,0 +1,9 @@
+from tkinter import *
+class New_Menu:
+    def __init__(self):
+        root = Tk()
+        root.title('Szachy')
+        root.geometry('400x400')
+        my_menu = Menu(root)
+
+        root.mainloop()
